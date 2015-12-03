99fdfe490e12194325ed8df84f23e0e5d8f22086
Juan Camilo Gamboa Higuera <juancamilog@gmail.com>
2015-12-02 22:08:04 -0500

    Tested code with sonar dataset

--- ghost/GP/GPRegressor.py
+++ ghost/GP/GPRegressor.py
@@ -1,6 +1,6 @@
 import os,sys
 from functools import partial
-from time import time
+from datetime import datetime
 
 import numpy as np
 import theano
@@ -37,7 +37,11 @@ def covSEard(loghyp,X1,X2=None):
         idims = X1.shape[0]
     X2 = X1 if X2 is None else X2
     D = maha(X1,X2,T.diag(T.exp(-2*loghyp[:idims])))
-    return T.exp(2*loghyp[idims] - 0.5*D)
+    #print theano.printing.debugprint(D)
+    #raw_input()
+
+    K = T.exp(2*loghyp[idims] - 0.5*D)
+    return K + 1e-4*T.log(n)*T.eye(n) if X1 is X2 else K
 
 def covNoise(loghyp,X1,X2=None,D=None):
     ''' Noise kernel. Takes as an input a distance matrix D and creates a new matrix 
@@ -60,12 +64,14 @@ class GP(object):
         self.name = name
         self.idims = X_dataset.shape[1]
         self.odims = Y_dataset.shape[1]
-        self.filename = '%s_%d_%d.zip'%(self.name,idims,odims)
+        self.filename = '%s_%d_%d_%s.zip'%(self.name,self.idims,self.odims,theano.config.device)
         self.should_save = False
 
         try:
             # try loading from pickled file, to avoid recompiling
             self.load()
+            if (X_dataset.shape[0] != self.X_.shape[0] or Y_dataset.shape[0] != self.Y_.shape[0]) or not( np.allclose(X_dataset,self.X_) and np.allclose(Y_dataset,self.Y_) ):
+                self.set_dataset(X_dataset,Y_dataset)
         
         except IOError:
             # initialize the class if no pickled version is available
@@ -75,10 +81,10 @@ class GP(object):
             self.init_predict()
             self.save()
         
-        print '[%f] %s > Finished initialising GP'%(time(),self.name)
+        print '[%s] %s > Finished initialising GP'%(str(datetime.now()),self.name)
     
     def set_dataset(self,X_dataset,Y_dataset):
-        print '[%f] %s > Updating GP dataset'%(time(),self.name)
+        print '[%s] %s > Updating GP dataset'%(str(datetime.now()),self.name)
         # ensure we don't change the number of input and output dimensions ( the number of samples can change)
         if self.X_ is not None:
             assert self.X_.shape[1] == X_dataset.shape[1]
@@ -87,9 +93,9 @@ class GP(object):
         # first, assign the numpy arrays to class members
         self.X_ = X_dataset
         self.Y_ = Y_dataset
-        idims = X_.shape[1]
-        odims = Y_.shape[1]
-        N = X_.shape[0]
+        idims = self.X_.shape[1]
+        odims = self.Y_.shape[1]
+        N = self.X_.shape[0]
         self.idims = idims
         self.odims = odims
         self.N = N
@@ -124,36 +130,51 @@ class GP(object):
         np.copyto(self.loghyp_,loghyp)
 
     def init_log_likelihood(self):
-        print '[%f] %s > Initialising expression graph for log likelihood'%(time(),self.name)
+        print '[%s] %s > Initialising expression graph for log likelihood'%(str(datetime.now()),self.name)
         idims = self.idims
         odims = self.odims
         # initialize the (before compilation) kernel function
         covs = (covSEard, covNoise)
         loghyps = [ (self.loghyp[i][:idims+1],self.loghyp[i][idims+1]) for i in xrange(odims) ]
         self.kernel_func = [ partial(covSum, loghyps[i], covs) for i in xrange(odims) ]
+        #for i in xrange(odims):
+            #print theano.printing.debugprint(loghyps[i][0])
+            #print theano.printing.debugprint(loghyps[i][1])
+            #raw_input()
 
         # We initialize the kernel matrices (one for each output dimension)
         self.K = [ self.kernel_func[i](self.X) for i in xrange(odims) ]
         self.iK = [ matrix_inverse(psd(self.K[i])) for i in xrange(odims) ]
+        self.iK = [ matrix_inverse(self.K[i]) for i in xrange(odims) ]
         self.beta = [ self.iK[i].dot(self.Y[:,i]) for i in xrange(odims) ]
 
+        #for i in xrange(odims):
+            #print theano.printing.debugprint(self.K[i])
+            #raw_input()
+        self.K_ = F((),self.K)
         # And finally, the negative log marginal likelihood ( again, one for each dimension; although we could share
         # the loghyperparameters across all output dimensions and train the GPs jointly)
         nlml = [ 0.5*(self.Y[:,i].T.dot(self.beta[i]) + T.log(det(psd(self.K[i]))) + self.N*T.log(2*np.pi) )/self.N  for i in xrange(odims) ]
+
+        #for i in xrange(odims):
+            #print theano.printing.debugprint(nlml[i])
+            #raw_input()
 
         # Compute the gradients for each output dimension independently
         dnlml = [ T.jacobian(nlml[i].flatten(),self.loghyp[i]) for i in xrange(odims)]
 
         # Compile the theano functions
-        print '[%f] %s > Compiling log likelihood'%(time(),self.name)
+        print '[%s] %s > Compiling log likelihood'%(str(datetime.now()),self.name)
         self.nlml = F((),nlml)
+        #print theano.printing.debugprint(self.nlml)
+        #raw_input()
         #self.nlml_i = [ F((),nlml[i]) for i in xrange(odims)]
-        print '[%f] %s > Compiling jacobian of log likelihood'%(time(),self.name)
+        print '[%s] %s > Compiling jacobian of log likelihood'%(str(datetime.now()),self.name)
         self.dnlml = F((),dnlml)
         #self.dnlml_i = [ F((),dnlml[i]) for i in xrange(odims)]
     
     def init_predict(self):
-        print '[%f] %s > Initialising expression graph for prediction'%(time(),self.name)
+        print '[%s] %s > Initialising expression graph for prediction'%(str(datetime.now()),self.name)
         odims = self.odims
         # and a expresion to evaluate the kernel vector at a new evaluation point
         x_mean = T.dmatrix('x')
@@ -167,40 +188,47 @@ class GP(object):
         # compile the prediction function
         M = T.stacklists(mean).T
         S = T.stacklists(variance).T
-        print '[%f] %s > Compiling mean and variance of prediction'%(time(),self.name)
+        print '[%s] %s > Compiling mean and variance of prediction'%(str(datetime.now()),self.name)
         self.predict_ = F([x_mean],(M,S))
 
         # compile the derivatives wrt the evaluation point
         dMdm = T.jacobian(M.flatten(),x_mean)
         dSdm = T.jacobian(S.flatten(),x_mean)
 
-        print '[%f] %s > Compiling derivatives of mean and variance of prediction'%(time(),self.name)
+        print '[%s] %s > Compiling derivatives of mean and variance of prediction'%(str(datetime.now()),self.name)
         self.predict_d_ = F([x_mean], (dMdm,dSdm))
     
     def predict(self,x_mean,x_cov = None):
         odims = self.odims
+        idims = self.idims
+        n = x_mean.shape[0]
         if len(x_mean.shape) == 1:
             # convert to row vector
             x_mean = x_mean[None,:]
-        res = None
 
-        if x_cov is None or self.name == 'GP':
-            print x_mean,x_cov
+        res = None
+        if self.name == 'GP':
             res = self.predict_(x_mean)
         else:
+            if x_cov is None:
+                x_cov = np.zeros((n,idims,idims))
             res = self.predict_(x_mean, x_cov)
-
         return res
 
     def predict_d(self,x_mean,x_cov=None):
         odims = self.odims
+        idims = self.idims
+        n = x_mean.shape[0]
         if len(x_mean.shape) == 1:
             # convert to row vector
             x_mean = x_mean[None,:]
         res = None
 
         if x_cov is None or self.name == 'GP':
-            res = self.predict_d_(x_mean)
+            if self.name == 'GP':
+                res = self.predict_(x_mean)
+            else:
+                res = self.predict_(x_mean, np.zeros((n_test,idims,idims)) )
         else:
             res = self.predict_d_(x_mean, x_cov)
         return res
@@ -211,27 +239,26 @@ class GP(object):
         nlml = sum(self.nlml())
         dnlml = np.array(self.dnlml()).flatten()
         #print nlml
+        #print np.exp(loghyp)
         return nlml,dnlml
 
     def train(self):
-        print '[%f] %s > Current hyperparameters:'%(time(),self.name)
         init_hyp = self.loghyp_.copy()
+        print '[%s] %s > Current hyperparameters:'%(str(datetime.now()),self.name)
         print np.exp(self.loghyp_)
-        print '[%f] %s > nlml:'%(time(),self.name)
-        print self.nlml()
+        print '[%s] %s > nlml: %s'%(str(datetime.now()),self.name, np.array(self.nlml()))
         opt_res = minimize(self.loss, self.loghyp_, jac=True, method="L-BFGS-B", tol=1e-12, options={'maxiter': 500})
         loghyp = opt_res.x.reshape(self.loghyp_.shape)
         self.should_save = not np.allclose(init_hyp,loghyp,1e-6,1e-9)
         np.copyto(self.loghyp_,loghyp)
         self.save()
-        print '[%f] %s > New hyperparameters:'%(time(),self.name)
+        print '[%s] %s > New hyperparameters:'%(str(datetime.now()),self.name)
         print np.exp(self.loghyp_)
-        print '[%f] %s > nlml:'%(time(),self.name)
-        print self.nlml()
+        print '[%s] %s > nlml: %s'%(str(datetime.now()),self.name, np.array(self.nlml()))
 
     def load(self):
         with open(self.filename,'rb') as f:
-            print '[%f] %s > Loading compiled GP with %d inputs and %d outputs'%(time(),self.name,self.idims,self.odims)
+            print '[%s] %s > Loading compiled GP with %d inputs and %d outputs'%(str(datetime.now()),self.name,self.idims,self.odims)
             state = t_load(f)
             self.X = state[0]
             self.Y = state[1]
@@ -247,7 +274,7 @@ class GP(object):
     def save(self):
         if self.should_save:
             with open(self.filename,'wb') as f:
-                print '[%f] %s > Saving compiled GP with %d inputs and %d outputs'%(time(),self.name,self.idims,self.odims)
+                print '[%s] %s > Saving compiled GP with %d inputs and %d outputs'%(str(datetime.now()),self.name,self.idims,self.odims)
                 state = (self.X,self.Y,self.loghyp,self.X_,self.Y_,self.loghyp_,self.nlml,self.dnlml,self.predict_,self.predict_d_)
                 t_dump(state,f,2)
 
@@ -256,7 +283,7 @@ class GPUncertainInputs(GP):
         super(GPUncertainInputs, self).__init__(X_dataset,Y_dataset,name=name)
 
     def init_predict(self):
-        print '[%f] %s > Initialising expression graph for prediction'%(time(),self.name)
+        print '[%s] %s > Initialising expression graph for prediction'%(str(datetime.now()),self.name)
         idims = self.idims
         odims = self.odims
 
@@ -265,7 +292,7 @@ class GPUncertainInputs(GP):
         x_cov = T.dtensor3('x_cov')  # n_samples x idims x idims
 
         #centralize inputs 
-        zeta, updts = theano.scan(fn=lambda x_mean_i: self.X - x_mean_i, sequences=x_mean)
+        zeta, updts = theano.scan(fn=lambda x_mean_i,X: X - x_mean_i, sequences=[x_mean], non_sequences=[self.X], strict=True)
         
         # predictive mean and covariance (for each output dimension)
         M = [] # mean
@@ -277,23 +304,23 @@ class GPUncertainInputs(GP):
             inp = zeta.dot(iL)
             # predictive mean ( which depends on input covariance )
             B = iL.dot(x_cov.T).transpose(2,0,1).dot(iL) + T.eye(idims)
-            t, updts = theano.scan(fn=lambda inp_k,B_k: inp_k.dot(matrix_inverse(B_k)),sequences=(inp,B))
+            t, updts = theano.scan(fn=lambda inp_k,B_k: inp_k.dot(matrix_inverse(B_k)),sequences=[inp,B])
             l = T.exp(-0.5*T.sum(inp*t,2))
             lb = l*self.beta[i] # beta should have been precomputed in init_log_likelihood
-            c, updts = theano.scan(fn=lambda B_k: T.exp(2*self.loghyp[i][idims])/T.sqrt(det(B_k)), sequences=B);
+            c, updts = theano.scan(fn=lambda B_k,log_sf: T.exp(2*log_sf)/T.sqrt(det(B_k)), sequences=[B], non_sequences=[self.loghyp[i][idims]], strict=True);
             mean = T.sum(lb,1)*c;
             M.append(mean)
 
             # inv(x_cov) times input output covariance
             tiL = t.dot(iL)
-            v, updts = theano.scan(fn=lambda tiL_k,lb_k,c_k: tiL_k.T.dot(lb_k)*c_k,sequences= (tiL,lb,c))
+            v, updts = theano.scan(fn=lambda tiL_k,lb_k,c_k: tiL_k.T.dot(lb_k)*c_k,sequences=[tiL,lb,c])
             V.append(v)
             
             #predictive second moment ( only the lower triangular part, including the diagonal)
-            def M2_helper(zeta_k,zeta_i_k, zeta_j_k, z_ij_k, R_k, x_cov_k):
+            def M2_helper(zeta_k,zeta_i_k, zeta_j_k, z_ij_k, R_k, x_cov_k, log_sf2_ij):
                 n_k = 0.5*(T.diag(zeta_k.dot(zeta_i_k.T))[:,None] + T.diag(zeta_k.dot(zeta_j_k.T))[None,:] - maha(z_ij_k,z_ij_k,matrix_inverse(psd(R_k)).dot(x_cov_k)))
                 t_k = 1.0/T.sqrt(det(psd(R_k)))
-                Q_k = t_k*T.exp( 2*(self.loghyp[i][idims] + self.loghyp[j][idims]) - n_k )
+                Q_k = t_k*T.exp( log_sf2_ij - n_k )
 
                 return Q_k
         
@@ -311,22 +338,21 @@ class GPUncertainInputs(GP):
 
                 R = x_cov.dot(Lambda) + T.eye(idims)
     
-                Q,updts = theano.scan(fn=M2_helper, sequences=(zeta,zeta_i,zeta_j,z_ij,R,x_cov) )
+                Q,updts = theano.scan(fn=M2_helper, sequences=(zeta,zeta_i,zeta_j,z_ij,R,x_cov), non_sequences=[2*(self.loghyp[i][idims] + self.loghyp[j][idims])], strict=True)
                 
                 # Eq 2.55
                 m2 = self.beta[i].dot(Q.T).T.dot(self.beta[j])
                 if i == j:
                     m2 =  m2 - T.sum(self.iK[i]*Q,(1,2)) + T.exp(2*self.loghyp[i][idims])
-
                 M2.append(m2)
-            
+
         M = T.stacklists(M).T
         V = T.stacklists(V).transpose(1,2,0)
         M2 = T.stacklists(M2).T
         S = M2 - (M[:,:,None]*M[:,None,:]).flatten(2)
 
-        print '[%f] %s > Compiling mean and variance of prediction'%(time(),self.name)
-        self.predict_ = F([x_mean,x_cov],(M,V,S))
+        print '[%s] %s > Compiling mean and variance of prediction'%(str(datetime.now()),self.name)
+        self.predict_ = F([x_mean,x_cov],(M,S,V))
 
         # compile the derivatives wrt the evaluation point
         dMdm = T.jacobian(M.flatten(),x_mean)
@@ -336,19 +362,19 @@ class GPUncertainInputs(GP):
         dVds = T.jacobian(V.flatten(),x_cov)
         dSds = T.jacobian(S.flatten(),x_cov)
 
-        print '[%f] %s > Compiling derivatives of mean and variance of prediction'%(time(),self.name)
-        self.predict_d_ = F([x_mean,x_cov], (dMdm,dVdm,dSdm,dMds,dVds,dSds))
+        print '[%s] %s > Compiling derivatives of mean and variance of prediction'%(str(datetime.now()),self.name)
+        self.predict_d_ = F([x_mean,x_cov], (dMdm,dMds,dSdm,dSds,dVdm,dVds))
 
-if __name__=='__main__':
+def test_random():
     # test function
     def f(X):
         #return X[:,0] + X[:,1]**2 + np.exp(-0.5*(np.sum(X**2,1)))
         return np.exp(-500*(np.sum(0.001*(X**2),1)))
 
-    n_samples = 50
+    n_samples = 500
     n_test = 10
-    idims = 7
-    odims = 10
+    idims = 2
+    odims = 1
 
     np.set_printoptions(linewidth=500)
     np.random.seed(31337)
@@ -358,8 +384,8 @@ if __name__=='__main__':
     for i in xrange(odims):
         Y_[:,i] =  (i+1)*f(X_) + 0.01*(np.random.rand(n_samples)-0.5)
     
-    gp = GP(X_,Y_)
-    gp.train()
+    #gp = GP(X_,Y_)
+    #gp.train()
 
     gpu = GPUncertainInputs(X_,Y_)
     gpu.train()
@@ -369,14 +395,60 @@ if __name__=='__main__':
     for i in xrange(odims):
         Y_[:,i] =  (i+1)*f(X_) + 0.01*(np.random.rand(n_test)-0.5)
 
-    r1 = gp.predict(X_,np.zeros((n_test,idims,idims)))
-    r2 = gpu.predict(X_,np.zeros((n_test,idims,idims)))
+    #r1 = gp.predict(X_,np.zeros((n_test,idims,idims)))
+    r2 = gpu.predict(X_)
 
     for i in xrange(n_test):
         print Y_[i,:],','
-        print r1[0][i],','
+     #   print r1[0][i],','
         print r2[0][i],','
 
-        print r1[1][i],','
-        print r2[2][i],','
+      #  print r1[1][i],','
+        print r2[1][i],','
         print '---'
+
+def test_sonar():
+    from scipy.io import loadmat
+    dataset = loadmat('/media/diskstation/Kingfisher/matlab.mat')
+    
+    Xd = np.array(dataset['mat'][:,0:2])
+    Xd += 1e-2*np.random.rand(*(Xd.shape))
+    Yd = np.array(dataset['mat'][:,2])[:,None]
+
+    gp = GP(Xd,Yd)
+    #gp = GPUncertainInputs(Xd,Yd)
+    #print '[%s] %s > training'%(str(datetime.now()),'main')
+    #gp.train()
+    #print '[%s] %s > done training'%(str(datetime.now()),'main')
+    
+    n_test=500
+    xg,yg = np.meshgrid ( np.linspace(Xd[:,0].min(),Xd[:,0].max(),n_test) , np.linspace(Xd[:,1].min(),Xd[:,1].max(),n_test) )
+    X_test= np.vstack((xg.flatten(),yg.flatten())).T
+    n = X_test.shape[0]
+    print '[%s] %s > predicting'%(str(datetime.now()),'main')
+
+    M = []; S = []
+    batch_size=5000
+    for i in xrange(0,n,batch_size):
+        next_i = min(i+batch_size,n)
+        print 'batch %d , %d'%(i,next_i)
+        r = gp.predict(X_test[i:next_i])
+        M.append(r[0])
+        S.append(r[1])
+
+    M = np.vstack(M)
+    S = np.vstack(S)
+
+    print '[%s] %s > done predicting'%(str(datetime.now()),'main')
+    from matplotlib import pyplot as plt
+    plt.figure()
+    plt.imshow(M.reshape(n_test,n_test), origin='lower')
+
+    plt.figure()
+    plt.imshow(S.reshape(n_test,n_test), origin='lower')
+    plt.show()
+
+
+if __name__=='__main__':
+    #test_random()
+    test_sonar()
--- ghost/GP/matlab_test.m
+++ ghost/GP/matlab_test.m
@@ -3,29 +3,27 @@ close all;
 
 rand('twister', 31337)
 try
-  rd = '/home/juancamilog/workspace/antoinette/pilcoV0.9/';
+  %rd = '/home/juancamilog/workspace/antoinette/pilcoV0.9/';
+  rd = '/home/thalassa/gamboa/gitrepo/tmp/antoinette/pilcoV0.9/';
   addpath([rd 'base'],[rd 'util'],[rd 'gp'],[rd 'control'],[rd 'loss']);
 catch
 end
 
-n = 100
+n = 500
 n_test = 10
 D = 2
-E = 3
+E = 2
 
 f = @(x) exp(-500*sum(0.001*x.^2,2));
 %m0 = randn(1,D)'
 %S0 = randn(D,D)'
 %S0 = eye(D);
 
-X = 10*(rand(D,n)' - 0.5)
-pause;
+X = 10*(rand(D,n)' - 0.5);
 Y = zeros(E,n)';
 for i=1:E
     Y(:,i) = i*f(X) + 0.01*(rand(1,n)' - 0.5);
 end
-Y
-pause;
 
 model.fcn = @gp0d;                % function for GP predictions
 model.train = @train;             % function to train dynamics model
@@ -37,14 +35,11 @@ model = model.train(model, [], trainOpt);  %  train dynamics GP
 exp(model.hyp')
 
 
-Xtest = 10*(rand(D,n_test)' -0.5)
-pause;
+Xtest = 10*(rand(D,n_test)' -0.5);
 Ytest = zeros(E,n_test)';
 for i=1:E
     Ytest(:,i) = i*f(Xtest) + 0.01*(rand(1,n_test)' - 0.5)
 end
-Ytest
-pause;
 
 for i=1:size(Ytest,1)
     disp('---')
