Fixed the covariance bug. Now we are getting the same results as the matlab implementation

ghost/learners/PILCO/pilco.py
@@ -96,8 +96,8 @@ class PILCO(EpisodicLearner):
             mx = mx + m_deltax
             D=mx.shape[1]
             Sx_deltax = (Sxu[:,:,:,None]*C_deltax[:,:,None,:]).sum(1)[:,:D,:]
-            print Sx_deltax
-            np.linalg.cholesky(Sx_deltax)
+            #print Sx_deltax
+            #np.linalg.cholesky(Sx_deltax)
             Sx = Sx + Sx_deltax + Sx_deltax.transpose(0,2,1) + S_deltax
             #  get cost:
             #mc, Sc = cost(mx,mu,Sx,Su)

ghost/regression/GPRegressor.py
@@ -191,7 +191,7 @@ class GP(object):
         for i in xrange(odims):
             k = self.kernel_func[i](m,self.X)
             mean[i] = k.dot(self.beta[i])
-            variance[i] = self.kernel_func[i](m,all_pairs=False) - (k*(k.dot( self.iK[i] )) ).sum(axis=1)
+            variance[i] = self.kernel_func[i](m,all_pairs=False) - (k*(k.dot( self.iK[i] )) ).sum(axis=1) #TODO verify this computation
 
         # compile the prediction function
         M = T.stack(mean).T
@@ -356,15 +356,16 @@ class GP_UI(GP):
             return (t_k,c_k)
             
         #predictive second moment ( only the lower triangular part, including the diagonal)
-        def M2_helper(logk_i_k, logk_j_k, z_ij_k, R_k, s_k):
-            nk2 = logk_i_k[:,None] + logk_j_k[None,:] - utils.maha(z_ij_k,z_ij_k,matrix_inverse(psd(R_k)).dot(s_k))
+        def M2_helper(logk_i_k, logk_j_k, z_i_k, z_j_k, R_k, s_k):
+            nk2 = logk_i_k[:,None] + logk_j_k[None,:] + utils.maha(z_i_k,-z_j_k,0.5*matrix_inverse(psd(R_k)).dot(s_k))
             tk = 1.0/T.sqrt(det(psd(R_k)))
-            Qk = tk*T.exp( nk2 )
+            Qk = T.exp( nk2 )
             
-            return Qk
+            return Qk,tk
 
         logk=[[]]*odims
         Lambda=[[]]*odims
+        z_=[[]]*odims
         for i in xrange(odims):
             # rescale input dimensions by inverse lengthscales
             iL = T.exp(-self.loghyp[i][:idims])
@@ -386,21 +387,22 @@ class GP_UI(GP):
             
             # predictive covariance
             logk[i] = 2*self.loghyp[i][idims] - 0.5*T.sum(inp*inp,2)
-            Lambda[i] = iL*iL
+            Lambda[i] = T.exp(-2*self.loghyp[i][:idims])
+            z_[i] = zeta*Lambda[i] 
             for j in xrange(i+1):
                 # This comes from Deisenroth's thesis ( Eqs 2.51- 2.54 )
-                z_ij = zeta*Lambda[i] + zeta*Lambda[j]
                 R = s*(Lambda[i] + Lambda[j]) + T.eye(idims)
     
-                Q,updts = theano.scan(fn=M2_helper, sequences=(logk[i],logk[j],z_ij,R,s))
+                (Q,t),updts = theano.scan(fn=M2_helper, sequences=(logk[i],logk[j],z_[i],z_[j],R,s))
                 Q.name = 'Q_%d%d'%(i,j)
 
                 # Eq 2.55
-                m2 = matrix_dot(self.beta[i],Q,self.beta[j])
+                m2 = self.beta[i].dot(Q).dot(self.beta[j].T)
                 if i == j:
                     iKi = self.iK[i].dot(T.eye(self.N))
-                    m2 =  m2 - T.sum(iKi*Q,(1,2)) + T.exp(2*self.loghyp[i][idims])
+                    m2 =  t*(m2 - T.sum(iKi*Q,(1,2))) + T.exp(2*self.loghyp[i][idims])
                 else:
+                    m2 = t*m2
                     M2[j*odims+i] = m2
                 m2.name = 'M2_%d%d'%(i,j)
                 M2[i*odims+j] = m2
@@ -765,15 +767,16 @@ class RBFGP(GP_UI):
             return (t_k,c_k)
             
         #predictive second moment ( only the lower triangular part, including the diagonal)
-        def M2_helper(logk_i_k, logk_j_k, z_ij_k, R_k, s_k):
-            nk2 = logk_i_k[:,None] + logk_j_k[None,:] - utils.maha(z_ij_k,z_ij_k,matrix_inverse(psd(R_k)).dot(s_k))
+        def M2_helper(logk_i_k, logk_j_k, z_i_k, z_j_k, R_k, s_k):
+            nk2 = logk_i_k[:,None] + logk_j_k[None,:] + utils.maha(z_i_k,-z_j_k,0.5*matrix_inverse(psd(R_k)).dot(s_k))
             tk = 1.0/T.sqrt(det(psd(R_k)))
-            Qk = tk*T.exp( nk2 )
+            Qk = T.exp( nk2 )
             
-            return Qk
+            return Qk,tk
 
         logk=[[]]*odims
         Lambda=[[]]*odims
+        z_=[[]]*odims
         for i in xrange(odims):
             # rescale input dimensions by inverse lengthscales
             iL = T.exp(-self.loghyp[i][:idims])
@@ -795,18 +798,17 @@ class RBFGP(GP_UI):
             
             # predictive covariance
             logk[i] = 2*self.loghyp[i][idims] - 0.5*T.sum(inp*inp,2)
-            Lambda[i] = iL*iL
+            Lambda[i] = T.exp(-2*self.loghyp[i][:idims])
+            z_[i] = zeta*Lambda[i] 
             for j in xrange(i+1):
                 # This comes from Deisenroth's thesis ( Eqs 2.51- 2.54 )
-                Lambda_ij = Lambda[i] + Lambda[j]
-                z_ij = zeta*Lambda_ij
-                R = s*Lambda_ij + T.eye(idims)
+                R = s*(Lambda[i] + Lambda[j]) + T.eye(idims)
     
-                Q,updts = theano.scan(fn=M2_helper, sequences=(logk[i],logk[j],z_ij,R,s))
+                (Q,t),updts = theano.scan(fn=M2_helper, sequences=(logk[i],logk[j],z_[i],z_[j],R,s))
                 Q.name = 'Q_%d%d'%(i,j)
 
                 # Eq 2.55
-                m2 = matrix_dot(self.beta[i],Q,self.beta[j])
+                m2 = self.beta[i].dot(Q).dot(self.beta[j].T)
                 m2.name = 'M2_%d%d'%(i,j)
                 if i != j:
                     M2[j*odims+i] = m2
@@ -815,8 +817,8 @@ class RBFGP(GP_UI):
         M = T.stack(M).T
         V = T.stack(V).transpose(1,2,0) # TODO if using angle dimensions, this will give the input-ouput covariances with the augmented inputs
         M2 = T.stack(M2).T
-        S = M2 - (M[:,:,None]*M[:,None,:]).flatten(2)
-        S ,updts = theano.scan(fn=lambda Si: Si.reshape((odims,odims)), sequences=[S], strict=True)
+        S ,updts = theano.scan(fn=lambda M2i: M2i.reshape((odims,odims)), sequences=[M2], strict=True)
+        S = S - (M[:,:,None]*M[:,None,:])
 
         utils.print_with_stamp('Compiling mean and variance of prediction',self.name)
         self.predict_ = F([x_mean,x_cov],(M,S,V), name='%s>predict_'%(self.name), profile=self.profile, mode=self.compile_mode)

test/testGPRegressor.py
@@ -30,6 +30,9 @@ def test_random(gp_type='GP'):
     gp.train()
     #gp.loghyp_[0] = np.array([1.437162320968175,1.455360729998610,-1.409930866260999,-5.823212436578583])
     #gp.loghyp_[1] = np.array([1.413559056853744,1.421240039262625,-0.781203990801477,-5.835053303554018])
+    #gp.loghyp_[0] = np.array([1.359377707437151e+00,1.364614810623883e+00,1.367879018497026e+00,1.364276703749956e+00,1.348163031453976e+00,-2.854374418091668e+00,-5.961433823748723e+00])
+    #gp.loghyp_[1] = np.array([1.373106876540056e+00,1.373825540199595e+00,1.367502939523581e+00,1.363373498722983e+00,1.367710094807602e+00,-2.220517946942496e+00,-5.990091744109144e+00])
+    #gp.loghyp_[2] = np.array([1.370114211425596e+00,1.370551817587214e+00,1.374419487066466e+00,1.364584339906947e+00,1.367756631930048e+00,-1.851265297500794e+00,-5.984429534701722e+00])
 
 
     Xd= 10*(np.random.rand(n_test,idims) - 0.5)
@@ -303,7 +306,7 @@ def test_angle():
 
 
 if __name__=='__main__':
-    np.set_printoptions(linewidth=500, precision=16, suppress=True)
+    np.set_printoptions(linewidth=500)#, precision=16, suppress=True)
     #test_random()
     #test_sonar('GP')
     #test_sonar('SPGP')
@@ -314,4 +317,4 @@ if __name__=='__main__':
     #test_K_means()
     #test_CartpoleDyn()
     #test_angle()
-    test_random()
+    test_random('GP_UI')