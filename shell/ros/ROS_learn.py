e969184e19dce1985ee516ba553ceaf4089aef12
achatr <achatr@localhost>
2016-05-31 09:55:13 -0400

    Added new files to ROS folder

--- shell/ros/ROS_learn.py
+++ shell/ros/ROS_learn.py
@@ -1,46 +1,44 @@
+import atexit
 import signal,sys
-sys.path.append('/home/adaptation/achatr/Desktop/Summer2016/PILCO_clone/kusanagi')
-import gym
+#sys.path.append('/home/adaptation/achatr/Desktop/Summer2016/PILCO_clone/kusanagi')
 import numpy as np
+from matplotlib import pyplot as plt
 from functools import partial
 from ghost.learners.PILCO import PILCO
 from shell.cartpole import Cartpole, CartpoleDraw, cartpole_loss
-from shell.plant import SerialPlant
 from ghost.control import RBFPolicy
-from examples.OAIgym.new_plant import Plant, OAIPlant
+from utils import plot_results
 
 if __name__ == '__main__':
     #np.random.seed(31337)
     np.set_printoptions(linewidth=500)
     # initliaze plant
-    dt = 0.02                                                         # simulation time step
+    dt = 0.1                                                         # simulation time step
     model_parameters ={}                                             # simulation parameters
-    model_parameters['l'] = 0.5                                      
-    model_parameters['m'] = 0.1
-    model_parameters['M'] = 1
+    model_parameters['l'] = 0.5
+    model_parameters['m'] = 0.5
+    model_parameters['M'] = 0.5
     model_parameters['b'] = 0.1
-    model_parameters['g'] = 9.8
-    x0 = [0,0,0,0]
+    model_parameters['g'] = 9.82
+    x0 = [0,0,0,0]                                                   # initial state mean
     S0 = np.eye(4)*(0.1**2)                                          # initial state covariance
     maxU = [10]
     measurement_noise = np.diag(np.ones(len(x0))*0.01**2)            # model measurement noise (randomizes the output of the plant)
-
-
-    env = gym.make('CartPole-v0')                                   # creates the cartpole visualization and environment
-    discrete = True
-    env.render()
-    plant = OAIPlant(discrete, model_parameters,x0,S0, dt, measurement_noise)
-    plant.setEnv(env)
-    x0, _ = plant.get_state()
+    plant = Cartpole(model_parameters,x0,S0,dt,measurement_noise)
+    draw_cp = CartpoleDraw(plant,0.033)                              # initializes visualization
+    draw_cp.start()
     
+    atexit.register(plant.stop)
+    atexit.register(draw_cp.stop)
+
     # initialize policy
-    angle_dims = [3]                                                
+    angle_dims = [3]
     policy = RBFPolicy(x0,S0,maxU,10, angle_dims)
 
     # initialize cost function
     cost_parameters = {}
     cost_parameters['angle_dims'] = angle_dims
-    cost_parameters['target'] = [0,0,0,0]                           
+    cost_parameters['target'] = [0,0,0,np.pi]
     cost_parameters['width'] = 0.25
     cost_parameters['expl'] = 0.0
     cost_parameters['pendulum_length'] = model_parameters['l']
@@ -48,28 +46,25 @@ if __name__ == '__main__':
 
     # initialize learner
     T = 4.0                                                          # controller horizon
-    J = 2                                                            # number of random initial trials
+    H_steps = np.ceil(T/dt)
+    J = 4                                                            # number of random initial trials
     N = 100                                                           # learning iterations
     learner = PILCO(plant, policy, cost, angle_dims, async_plant=False)
-    
-    def signal_handler(signal, frame):                               # initialize signal handler to capture ctrl-c
-        print 'Caught CTRL-C!'
-        #draw_cp.stop()
-        #plant.stop()
-        sys.exit(0)
-    signal.signal(signal.SIGINT, signal_handler)
 
     if learner.dynamics_model.X_ is None: #if we have no prior data
         # gather data with random trials
         for i in xrange(J):
             plant.reset_state()
             learner.apply_controller(H=T,random_controls=True)
- #   else:
- #       plant.reset_state()
- 
-    plant.reset_state()
-    learner.apply_controller(H=T)
+    else:
+        plant.reset_state()
+        experience_data = learner.apply_controller(H=T)
         
+        # plot results
+        learner.init_rollout(derivs=False)
+        plot_results(learner, x0, S0, H_steps)
+
+    # learning loop
     for i in xrange(N):
         # train the dynamics models given the collected data
         learner.train_dynamics()
@@ -77,11 +72,14 @@ if __name__ == '__main__':
         # train policy
         learner.train_policy(H=T)
 
-        # execute it on the robot564,
+        # execute it on the robot
         plant.reset_state()
-        learner.apply_controller(H=T)
+        experience_data = learner.apply_controller(H=T)
+
+        # plot results
+        plot_results(learner, x0, S0, H_steps)
 
         # save latest state of the learner
         learner.save()
     
-    draw_cp.stop()
+    sys.exit(0)
