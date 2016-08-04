a14ac53e67f112ea326ed97277450d5462689e51
Juan Camilo Gamboa Higuera <juancamilog@gmail.com>
2016-08-04 14:00:15 -0400

    fixed bug in the get_params method of SSGP_UI

--- ghost/regression/GP.py
+++ ghost/regression/GP.py
@@ -998,14 +998,11 @@ class SSGP(GP):
             self.w.set_value(w,borrow=True)
     
     def get_params(self, symbolic=True, all_shared=False):
-        retvars = super(SSGP,self).get_params(symbolic=False)
+        retvars = super(SSGP,self).get_params(symbolic=True)
         retvars.append(self.w)
         if not symbolic:
-            try:
-                retvars1 = [ r.get_value(borrow=True) for r in retvars]
-                return retvars1
-            except AttributeError:
-                return retvars
+            retvars = [ r.get_value(borrow=True) for r in retvars]
+            return retvars
 
     def loss_ss(self, params, parameter_shapes):
         loghyp,w = utils.unwrap_params(params,parameter_shapes)
