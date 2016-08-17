f6fdad9b8d24f94f5cfff8a871e1d364d2ea0ae0
Anqi Xu (930-Ubuntu) <Anqi Xu (930-Ubuntu)@localhost>
2016-08-17 08:50:43 -0400

    stronger pLAUts

--- diagnosis_tools/PLOT_LEARNING_SERVER_PROFILES.m
+++ diagnosis_tools/PLOT_LEARNING_SERVER_PROFILES.m
@@ -3,7 +3,7 @@
 logs = {'inferno', 'gorgona', 'ithaca', 'moist', 'numenor', 'capilano'};
 for j = 1:length(logs),
     name = logs{j};
-    csv_file = sprintf('/media/diskstation/learn_to_swim/DELETE_ME/anqi_test_remotes/uturn_%s_timings.csv', name);
-    plot_learning_server_profile(j, name, csv_file);
-    print(gcf, '-dpng', sprintf('uturn_%s_timings.png', name));
+    csvpath = sprintf('/media/diskstation/learn_to_swim/DELETE_ME/anqi_test_remotes/uturn_%s_timings.csv', name);
+    pngpath = sprintf('/media/diskstation/learn_to_swim/DELETE_ME/anqi_test_remotes/uturn_%s_timings.png', name);
+    plot_learning_server_profile(name, csvpath, j, pngpath);
 end;
--- diagnosis_tools/plot_learning_server_profile.m
+++ diagnosis_tools/plot_learning_server_profile.m
@@ -1,14 +1,24 @@
-function [] = plot_learning_server_profile(j, name, csv_file)
+function [] = plot_learning_server_profile(name, csvpath, fig_j, pngpath)
 
 data = csvread(csv_file, 3);
 NUM_ITERS = size(data, 1);
 
-figure(j);
+if nargin < 3,
+  h_fig = figure();
+else
+  h_fig = figure(fig_j);
+end
+
 clf;
-bar(1:NUM_ITERS, data(:, 2:4)/60, 0.5, 'stack');
+bar(1:NUM_ITERS, data(:, 2:5)/60, 0.5, 'stack');
 title(sprintf('Timings for %s', name));
 xlabel('Episode');
 ylabel('Duration (min)');
 legend({'Dynamics', 'Policy', 'Save'}, 'Location', 'NorthWest');
+grid on;
+
+if nargin >= 4,
+  print(h_fig, '-dpng', pngpath);
+end
 
 end
