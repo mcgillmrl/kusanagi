e1782c1e18ee01860e46ad0b33cc0c195827b0a0
Juan Camilo Gamboa Higuera <juancamilog@gmail.com>
2016-08-18 03:08:43 -0400

    Minor fix

--- ghost/learners/EpisodicLearner.py
+++ ghost/learners/EpisodicLearner.py
@@ -159,6 +159,8 @@ class EpisodicLearner(Loadable):
     
     def set_cost(self, new_cost_func, new_cost_params):
         ''' Replaces the old cost function with a new one (and recompiles it)'''
+        if 'angle_dims' not in new_cost_params:
+            new_cost_params['angle_dims'] = self.angle_idims
         utils.print_with_stamp('Cost parameters: %s'%(new_cost_params),self.name)
         if self.cost is not None:
             if self.cost.func == new_cost_func and self.cost.keywords['params'] == new_cost_params:
