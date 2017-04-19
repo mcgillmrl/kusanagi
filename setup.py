ea4347244475cf9c4d27131c5e64c50995a106cf
Juan Camilo Gamboa Higuera <juancamilog@gmail.com>
2017-04-18 21:09:09 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""
+ Setup tools configuration for installing this package
+"""
+from setuptools import setup, find_packages
+
+setup(name='kusanagi',
+      version='0.1',
+      description='A modular RL library',
+      url='http://github.com/juancamilog/kusanagi',
+      author='Juan Camilo Gamboa Higuera',
+      author_email='juancamilog@gmail.com',
+      license='MIT',
+      packages=find_packages(exclude=['examples', 'thirdparty', 'doc', 'test']),
+      install_requires=['theano', 'lasagne', 'pyserial'],
+     )
