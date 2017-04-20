fix for empirical initial state covariance

kusanagi/ghost/learners/EpisodicLearner.py
@@ -406,8 +406,8 @@ class EpisodicLearner(Loadable):
                 self.n_evals+=1
                 gmag = [np.sqrt((p**2).sum()) for p in dJdp]
                 gmax = [p.max() for p in dJdp]
-                utils.print_with_stamp('Current value: %E, Total evaluations: \
-                %d, gm: %s, lr: %f'%(v, self.n_evals, gmag, lr), self.name,True)
+                out_str = 'Current value: %E, Total evaluations: %d, gm: %s, lr: %f'
+                utils.print_with_stamp(out_str%(v, self.n_evals, gmag, lr), self.name,True)
         # probabilistic line searches
         elif min_method == 'PROBLS':
             if not hasattr(self,'loss_fn'):

kusanagi/ghost/learners/PILCO/pilco.py
@@ -438,7 +438,8 @@ class PILCO(EpisodicLearner):
         if self.use_empirical_x0:
             if n_episodes > 3:
                 self.mx0.set_value(x0.mean(0).astype(theano.config.floatX))
-                self.Sx0.set_value(np.cov(x0.T, ddof=1).astype(theano.config.floatX))
+                cov0 = np.diag(np.diag(np.cov(x0.T, ddof=1).astype(theano.config.floatX)))
+                self.Sx0.set_value(cov0)
         #print x0
         #print self.Sx0.get_value()
 