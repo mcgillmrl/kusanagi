d0450df991c8074ed162d2f7155c8ef6532c2f57
Juan Camilo Gamboa Higuera <juancamilog@gmail.com>
2017-08-02 15:31:23 -0400

    minor bug fixes

--- kusanagi/ghost/control/NNPolicy.py
+++ kusanagi/ghost/control/NNPolicy.py
@@ -1,44 +1,53 @@
 import lasagne
 import numpy as np
 import theano
-import theano.tensor as tt
 
 from kusanagi.ghost.regression import BNN
-from kusanagi.ghost.control.saturation import gSat, tanhSat
+from kusanagi.ghost.control.saturation import tanhSat as sat
 from functools import partial
+
 
 # NN controller
 class NNPolicy(BNN):
-    def __init__(self, m0, maxU=[10], angle_dims=[], sat_func=gSat, name='NNPolicy', filename=None, **kwargs):
+    def __init__(self, m0, maxU=[10], angle_dims=[], sat_func=sat,
+                 name='NNPolicy', filename=None, **kwargs):
         self.maxU = np.array(maxU, dtype=theano.config.floatX)
         self.D = np.array(m0).size + len(angle_dims)
         self.E = len(maxU)
 
         if sat_func:
             self.sat_func = partial(sat_func, e=self.maxU)
-        
-        print(type(self), isinstance(self,NNPolicy))
-        super(NNPolicy,self).__init__(self.D, self.E, name=name, filename=filename, **kwargs)
+
+        print(type(self), isinstance(self, NNPolicy))
+        super(NNPolicy, self).__init__(self.D, self.E, name=name,
+                                       filename=filename, **kwargs)
 
     def get_params(self, symbolic=True):
         if symbolic:
-            return lasagne.layers.get_all_params(self.network,trainable=True)
+            return lasagne.layers.get_all_params(self.network,
+                                                 trainable=True)
         else:
-            return lasagne.layers.get_all_param_values(self.network,trainable=True)
+            return lasagne.layers.get_all_param_values(self.network,
+                                                       trainable=True)
 
-    def set_params(self,params):
-        lasagne.layers.set_all_param_values(self.network,params,trainable=True)
+    def set_params(self, params):
+        lasagne.layers.set_all_param_values(self.network, params,
+                                            trainable=True)
 
-    def predict_symbolic(self,mx,Sx=None,**kwargs):
+    def predict_symbolic(self, mx, Sx=None, **kwargs):
         if self.network_spec is None:
-            self.network_spec = self.get_default_network_spec(input_dims=self.D,
-                                                      output_dims=self.E,
-                                                      hidden_dims=[100, 100, 100, 100],
-                                                      nonlinearities=lasagne.nonlinearities.elu,
-                                                      p=0.05, name=self.name)
+            self.network_spec = self.get_default_network_spec(
+                input_dims=self.D,
+                output_dims=self.E,
+                hidden_dims=[100],
+                nonlinearities=lasagne.nonlinearities.elu,
+                p=0.0, name=self.name)
 
         if self.network is None:
-            params = self.network_params if self.network_params is not None else {}
+            params = self.network_params\
+                     if self.network_params is not None\
+                     else {}
+
             self.network = self.build_network(self.network_spec,
                                               params=params,
                                               name=self.name)
@@ -47,7 +56,7 @@ class NNPolicy(BNN):
 
         if Sx is None:
             if isinstance(ret, list):
-                ret=ret[0]
+                ret = ret[0]
             M = ret
             if self.sat_func is not None:
                 # saturate the output
@@ -58,19 +67,19 @@ class NNPolicy(BNN):
             # apply saturating function to the output if available
             if self.sat_func is not None:
                 # saturate the output
-                M,S,U = self.sat_func(M,S)
+                M, S, U = self.sat_func(M, S)
                 # compute the joint input output covariance
                 V = V.dot(U)
             return M, S, V
 
     def evaluate(self, m, s=None, t=None, symbolic=False, **kwargs):
-        D = m.shape[0]
         if symbolic:
-            # by default, sample internal params (e.g. dropout masks) at every evaluation
-            kwargs['iid_per_eval'] = kwargs.get('iid_per_eval',True)
-            
-            ret = self.predict_symbolic(m,s,**kwargs)
+            # by default, sample internal params (e.g. dropout masks)
+            # at every evaluation
+            kwargs['iid_per_eval'] = kwargs.get('iid_per_eval', True)
+
+            ret = self.predict_symbolic(m, s, **kwargs)
             theano.printing.Print('ret')(ret)
         else:
-            ret = self.predict(m,s)
-        return ret 
+            ret = self.predict(m, s)
+        return ret
