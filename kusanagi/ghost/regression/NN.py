94eb68e019390e3da5ed51fa5b156e54cb4643c6
Juan Camilo Gamboa Higuera <juancamilog@gmail.com>
2017-09-01 23:17:19 -0400

    Changed default nn architecture

--- kusanagi/ghost/regression/NN.py
+++ kusanagi/ghost/regression/NN.py
@@ -8,31 +8,15 @@ import kusanagi
 from collections import OrderedDict
 from lasagne import nonlinearities
 from lasagne.random import get_rng
+from numbers import Number
 from theano.sandbox.rng_mrg import MRG_RandomStreams as RandomStreams
 
 from kusanagi import utils
 from kusanagi.ghost.optimizers import SGDOptimizer
 from kusanagi.ghost.regression import BaseRegressor
+from kusanagi.ghost.regression import objectives
 
 floatX = theano.config.floatX
-
-
-def gaussian_log_likelihood(inputs, targets, 
-                            pred_mean, pred_std=None):
-    ''' Computes the eempirical expected value of the log likelihood,
-    for a gaussian distributed predictions. This assumes diagonal covariances
-    '''
-    delta = pred_mean - targets
-    # note that if we have nois be a 1xD vector, broadcasting
-    # rules apply
-    if pred_std:
-        lml = tt.square(delta/pred_std).sum(-1) + tt.log(pred_std).sum(-1)
-    else:
-        lml = tt.square(delta).sum(-1)
-
-    E_lml = -0.5*lml.mean()
-
-    return E_lml
 
 
 class BNN(BaseRegressor):
@@ -174,15 +158,15 @@ class BNN(BaseRegressor):
 
     def get_default_network_spec(self, batchsize=None, input_dims=None,
                                  output_dims=None,
-                                 hidden_dims=[400]*3,
-                                 p=0.1, p_input=0.0,
+                                 hidden_dims=[200]*3,
+                                 p=0.5, p_input=0.01,
                                  nonlinearities=nonlinearities.rectify,
                                  output_nonlinearity=nonlinearities.linear,
                                  W_init=lasagne.init.Orthogonal(gain='relu'),
                                  b_init=lasagne.init.Constant(0.),
                                  name=None):
         from lasagne.layers import InputLayer, DenseLayer
-        from kusanagi.ghost.regression.layers import DropoutLayer
+        from kusanagi.ghost.regression.layers import DenseGaussianDropoutLayer as DenseDropoutLayer
         if name is None:
             name = self.name
         if input_dims is None:
@@ -190,46 +174,39 @@ class BNN(BaseRegressor):
         if output_dims is None:
             output_dims = 2*self.E if self.heteroscedastic else self.E
         if not isinstance(p, list):
-            p = [p]*len(hidden_dims)
+            p = [p]*(len(hidden_dims))
         if not isinstance(nonlinearities, list):
             nonlinearities = [nonlinearities]*len(hidden_dims)
         if not isinstance(W_init, list):
             W_init = [W_init]*(len(hidden_dims)+1)
         if not isinstance(b_init, list):
             b_init = [b_init]*(len(hidden_dims)+1)
-        n_samples = self.n_samples.get_value()
         network_spec = []
         # input layer
         input_shape = (batchsize, input_dims)
         network_spec.append((InputLayer,
                              dict(shape=input_shape,
                                   name=name+'_input')))
-        if p_input > 0:
-            network_spec.append((DropoutLayer,
-                                 dict(p=p_input,
-                                      rescale=False,
-                                      name=name+'_drop_input',
-                                      n_samples=n_samples)))
+
         # hidden layers
+        p = [p_input] + p
         for i in range(len(hidden_dims)):
-            network_spec.append((DenseLayer,
+            layer_type = DenseLayer if p == 0 else DenseDropoutLayer
+            network_spec.append((layer_type,
                                  dict(num_units=hidden_dims[i],
                                       nonlinearity=nonlinearities[i],
                                       W=W_init[i],
                                       b=b_init[i],
+                                      p=p[i],
                                       name=name+'_fc%d' % (i))))
-            if p[i] > 0:
-                network_spec.append((DropoutLayer,
-                                     dict(p=p[i],
-                                          rescale=False,
-                                          name=name+'_drop%d' % (i),
-                                          n_samples=n_samples)))
         # output layer
-        network_spec.append((DenseLayer,
+        layer_type = DenseLayer if p == 0 else DenseDropoutLayer
+        network_spec.append((layer_type,
                              dict(num_units=output_dims,
                                   nonlinearity=output_nonlinearity,
                                   W=W_init[-1],
                                   b=b_init[-1],
+                                  p=p[-1],
                                   name=name+'_output')))
 
         return network_spec
@@ -319,82 +296,46 @@ class BNN(BaseRegressor):
             iid_per_eval=True, return_samples=True)
 
         # build the dropout loss function ( See Gal and Ghahramani 2015)
-        deltay = train_predictions - train_targets
-        M, E = train_targets.shape
-        N = self.X.shape[0]
-        N = M.astype(theano.config.floatX)
-        M = M.astype(theano.config.floatX)
-        E = E.astype(theano.config.floatX)
+        M = train_targets.shape[0].astype(theano.config.floatX)
+        N = self.X.shape[0].astype(theano.config.floatX)
 
         # compute negative log likelihood
         # note that if we have sn_std be a 1xD vector, broadcasting
         # rules apply
-        nlml = 0.5*tt.square(deltay/sn).sum(-1) + tt.log(sn).sum(-1)
-        loss = nlml.sum()
+        lml = objectives.gaussian_log_likelihood(
+            train_targets, train_predictions, sn)
+        loss = -lml.sum()
 
         # compute regularization term
-        loss += self.get_regularization_term(
-            input_lengthscale, hidden_lengthscale)*M/N
+        # this is only for binary dropout layers
+        loss += objectives.dropout_gp_kl(
+            self.network, input_lengthscale, hidden_lengthscale)*M/N
+        # this is only for gaussian dropout layers
+        loss += objectives.gaussian_dropout_kl(self.network)*M/N
 
         inputs = [train_inputs, train_targets,
                   input_lengthscale, hidden_lengthscale]
-        updates = {}
+        updates = theano.updates.OrderedUpdates()
 
         # get trainable network parameters
         params = lasagne.layers.get_all_params(self.network, trainable=True)
+
         # if we are learning the noise
         if not self.heteroscedastic:
             params.append(self.unconstrained_sn)
         self.set_params(dict([(p.name, p) for p in params]))
         return loss, inputs, updates
 
-    def get_regularization_term(self, input_lengthscale=1,
-                                hidden_lengthscale=1):
-        reg = 0
-        layers = lasagne.layers.get_all_layers(self.network)
-
-        for i in range(1, len(layers)):
-            reg_weight = 0.5
-            # apply different regularization weigths to the input,
-            # and the hidden dimension
-            is_dropout = isinstance(layers[i-1],
-                                    lasagne.layers.DropoutLayer)
-            ind = i if not (i > 1 and is_dropout) else i-1
-
-            is_input = isinstance(layers[ind].input_layer,
-                                  lasagne.layers.InputLayer)
-
-            if hasattr(layers[ind], 'input_layer') and is_input:
-                reg_weight *= input_lengthscale**2
-            else:
-                reg_weight *= hidden_lengthscale**2
-
-            # if this layer has a weight layer and the previous layer
-            # is a DropoutLayer
-            if hasattr(layers[i], 'W'):
-                if i > 1 and is_dropout:
-                    p = layers[i-1].p
-                    if p > 0:
-                        reg_weight *= (1-p)
-                reg += reg_weight*lasagne.regularization.l2(layers[i].W)
-
-            if hasattr(layers[i], 'b') and layers[i].b is not None:
-                reg += reg_weight*lasagne.regularization.l2(layers[i].b)
-
-        return reg
-
     def get_updates(self, network=None):
-        ''' returns a dictionary where the keys are lasagne layer instances
-            and the values are their corresponding dropout masks'''
+        ''' returns an updates dictionary, collected from layers in the
+        networks that provide the get_updates method'''
         if network is None:
             network = self.network
-        mask_updates = []
+        layer_updates = theano.updates.OrderedUpdates()
         for l in lasagne.layers.get_all_layers(network):
-            if isinstance(l, kusanagi.ghost.regression.layers.DropoutLayer)\
-               and l.mask_updates is not None:
-                mask_updates.append((l.mask, l.mask_updates))
-
-        return mask_updates
+            if hasattr(l, 'get_updates'):
+                layer_updates += l.get_updates()
+        return layer_updates
 
     def predict_symbolic(self, mx, Sx=None, deterministic=False,
                          iid_per_eval=False, return_samples=False,
@@ -419,11 +360,11 @@ class BNN(BaseRegressor):
             # standardize inputs
             x = (x - self.Xm).dot(self.iXs)
 
-        # unless we set the shared_axes parameter on the droput layers,
-        # the dropout masks should be different per sample
+        # unless we set the shared_axes parameter on the dropout layers,
+        # the noise samples should be different per input sample
         ret = lasagne.layers.get_output(self.network, x,
                                         deterministic=deterministic,
-                                        fixed_dropout_masks=not iid_per_eval)
+                                        fixed_noise_samples=not iid_per_eval)
         y = ret[:, :self.E]
         sn = (tt.nnet.softplus(ret[:, self.E:])
               if self.heteroscedastic
@@ -432,8 +373,8 @@ class BNN(BaseRegressor):
         if whiten_outputs and hasattr(self, 'Ym') and self.Ym is not None:
             # scale and center outputs
             y = y.dot(self.Ys) + self.Ym
-            # rescale noise variance
-            # sn = sn.dot(self.Ys)
+            # rescale variances
+            #sn = sn.dot(self.Ys)
 
         y.name = '%s>output_samples' % (self.name)
         if return_samples:
@@ -467,11 +408,11 @@ class BNN(BaseRegressor):
 
             # increase the size of the masks
             updts = self.get_updates()
-            for mask, updt in updts:
-                mask_shape = mask.shape.eval()
-                if mask_shape[0] != n_samples:
-                    mask_shape[0] = n_samples
-                    mask.set_value(np.zeros(mask_shape, dtype=mask.dtype))
+            for v, updt in updts.items():
+                v_shape = v.shape.eval()
+                if v_shape[0] != n_samples:
+                    v_shape[0] = n_samples
+                    v.set_value(np.zeros(v_shape, dtype=v.dtype))
 
         if not hasattr(self, 'update_fn') or self.update_fn is None:
             # get prediction with non deterministic samples
@@ -488,7 +429,7 @@ class BNN(BaseRegressor):
         self.update_fn()
 
     def train(self, batch_size=100,
-              input_ls=None, hidden_ls=None, lr=1e-4,
+              input_ls=None, hidden_ls=None, lr=1e-3,
               optimizer=None, callback=None):
         if optimizer is None:
             optimizer = self.optimizer
@@ -513,150 +454,5 @@ class BNN(BaseRegressor):
                                      input_ls, hidden_ls, lr,
                                      batch_size=batch_size)
         self.trained = True
+        self.update()
 
-
-def build_mlp(input_dims,
-              output_dims,
-              batchsize=None,
-              hidden_dims=[200, 200],
-              nonlin=lasagne.nonlinearities.elu,
-              W_init=lasagne.init.GlorotUniform(),
-              b_init=lasagne.init.Constant(0.),
-              output_nonlin=lasagne.nonlinearities.linear,
-              name=None):
-    if name is None:
-        name = 'mlp'
-    if not isinstance(input_dims, list):
-        input_dims = [input_dims]
-    if not isinstance(nonlin, list):
-        nonlin = [nonlin]*len(hidden_dims)
-    if not isinstance(W_init, list):
-        W_init = [W_init]*(len(hidden_dims)+1)
-    if not isinstance(b_init, list):
-        b_init = [b_init]*(len(hidden_dims)+1)
-
-    # input layer
-    input_dims = [batchsize] + input_dims
-    net = lasagne.layers.InputLayer(
-        input_dims, name=name+'_input')
-
-    # hidden layers
-    for i in range(len(hidden_dims)):
-        net = lasagne.layers.DenseLayer(
-            net, hidden_dims[i], W_init[i], b_init[i], nonlin[i],
-            name=name+"_fc%d" % i)
-
-    # output layer
-    net = lasagne.layers.DenseLayer(
-        net, output_dims, W_init[-1], b_init[-1], output_nonlin,
-        name=name+"_output")
-
-    return net
-
-
-def whiten(x, mean, inv_cov):
-    '''
-        Transforms input data to have zero mean, and identity covariance.
-    '''
-    if inv_cov.ndim != 2:
-        return (x - mean)*inv_cov
-    else:
-        return (x - mean).dot(inv_cov)
-
-
-class NormalInit(object):
-    def __init__(self, mean=0, std=1,
-                 init_mean=lasagne.init.GlorotUniform(),
-                 rng=RandomStreams(get_rng().randint(1, 2147462579)),
-                 name='Normal'):
-        self.mean = mean
-        self.std = std
-        self.rng = rng
-        self.init_mean = init_mean
-        self.updates = OrderedDict()
-        self.name = name
-        self.count = 0
-
-    def __call__(self, shape):
-        # create random variable
-        z = theano.shared(
-            floatX(np.random.normal(self.mean, self.std, size=shape)),
-            name='%s_z_%d' % (self.name, self.count))
-        z_updt = self.rng.normal(shape)
-        # we will use this to control exactly when we should draw new samples
-        self.updates[z] = z_updt
-
-        # create params for mean and covariance
-        # mean initialized around 0
-        W_mean = theano.shared(
-            self.init_mean(shape), name='%s_mean_%d' % (self.name, self.count))
-        # variances initialized around 1
-        W_logstd = theano.shared(
-            np.random.normal(1, 0.05, size=shape),
-            name='%s_std_%d' % (self.name, self.count))
-
-        self.count += 1
-
-        return W_mean + tt.nnet.softplus(W_logstd)*z
-
-
-class BBB_NN(BaseRegressor):
-    def __init__(self, idims, odims, network_init=build_mlp, n_samples=25,
-                 heteroscedastic=False, name='BNN',
-                 filename=None, **kwargs):
-        self.D = idims
-        self.E = odims
-        utils.print_with_stamp("Initializing network", name)
-        self.network = network_init(
-            W_init=NormalInit(name=self.name+'_W'))
-
-        # number of samples for the monte carlo integration
-        samples = np.array(n_samples).astype('int32')
-        samples_name = "%s>n_samples" % (self.name)
-        self.n_samples = theano.shared(samples, name=samples_name)
-
-        self.heteroscedastic = heteroscedastic
-        self.name = name
-        self.filename = filename
-
-        # output measurement noise
-        if not self.heteroscedastic:
-            sn = (np.ones((self.E,))*1e-3).astype(floatX)
-            sn = np.log(np.exp(sn)-1)
-            self.unconstrained_sn = theano.shared(
-                sn, name='%s_sn' % (self.name))
-            # contrained to positive values
-            eps = np.finfo(np.__dict__[floatX]).eps
-            self.sn = tt.nnet.softplus(self.unconstrained_sn) + eps
-
-        # random number generator
-        seed = lasagne.random.get_rng().randint(1, 2147462579)
-        self.m_rng = theano.sandbox.rng_mrg.MRG_RandomStreams(seed)
-
-        # filename for saving
-        fname = '%s_%d_%d_%s_%s' % (self.name, self.D, self.E,
-                                    theano.config.device,
-                                    theano.config.floatX)
-        self.filename = fname if filename is None else filename
-        BaseRegressor.__init__(self, name=name, filename=self.filename)
-        if filename is not None:
-            self.load()
-
-    def set_dataset(self, X_dataset, Y_dataset, **kwargs):
-        # set dataset
-        super(BNN, self).set_dataset(X_dataset.astype(floatX),
-                                     Y_dataset.astype(floatX))
-
-        # extra operations when setting the dataset (specific to this class)
-        self.update_dataset_statistics(X_dataset, Y_dataset)
-
-        if not self.heteroscedastic and not self.trained:
-            # default log of measurement noise std is set to 5% of
-            # dataset variation
-            s = (0.05*Y_dataset.std(0)).astype(floatX)
-            s = np.log(np.exp(s, dtype=floatX)-1.0)
-            self.unconstrained_sn.set_value(s)
-
-    def get_loss(self):
-        ''' initializes the loss function for training '''
-        pass
