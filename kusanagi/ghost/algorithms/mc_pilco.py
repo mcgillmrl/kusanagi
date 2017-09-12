1f579fdf0809853d7609fa9fb726db3e36267f12
Juan Camilo Gamboa Higuera <juancamilog@gmail.com>
2017-09-12 00:38:06 -0400

    updated mcpilco to use common randdon numbers

--- kusanagi/ghost/algorithms/mc_pilco.py
+++ kusanagi/ghost/algorithms/mc_pilco.py
@@ -39,7 +39,7 @@ def propagate_particles(x, pol, dyn, D, angle_dims=None, iid_per_eval=False):
 def rollout(x0, H, gamma0,
             pol, dyn, cost,
             D, angle_dims=None, resample=True,
-            truncate_gradient=-1,
+            z=None, truncate_gradient=-1,
             **kwargs):
     ''' Given some initial state particles x0, and a prediction horizon H
     (number of timesteps), returns a set of trajectories sampled from the
@@ -50,28 +50,26 @@ def rollout(x0, H, gamma0,
     utils.print_with_stamp(msg, 'mc_pilco.rollout')
 
     # define internal scan computations
-    def step_rollout(x, gamma, *args):
+    def step_rollout(z, x, gamma, *args):
         '''
             Single step of rollout.
         '''
         # get next state distribution
         x_next, sn = propagate_particles(x, pol, dyn, D, angle_dims, **kwargs)
 
-        x_next_noisy = x_next + sn*m_rng.normal(x_next.shape) 
         #  get cost of applying action:
         n = x_next.shape[0]
         n = n.astype(theano.config.floatX)
         mx_next = x_next.mean(0)
         Sx_next = x_next.T.dot(x_next)/n - tt.outer(mx_next, mx_next)
         mc_next = cost(mx_next, Sx_next)[0]
+        c_next = cost(x_next, None)
+        #mc_next = c_next.mean(0)
 
         # resample if requested
         if resample:
-            z = m_rng.normal(x_next.shape)
+            #z = m_rng.normal(x_next.shape)
             x_next = mx_next + z.dot(tt.slinalg.cholesky(Sx_next).T)
-
-        c_next = cost(x_next, None)
-        #mc_next = c_next.mean(0)
 
         return [gamma*mc_next, gamma*c_next, x_next, gamma*gamma0]
 
@@ -84,6 +82,7 @@ def rollout(x0, H, gamma0,
 
     # loop over the planning horizon
     output = theano.scan(fn=step_rollout,
+                         sequences=[z],
                          outputs_info=[None, None, x0, gamma0],
                          non_sequences=nseq,
                          n_steps=H,
@@ -104,7 +103,7 @@ def rollout(x0, H, gamma0,
 
 
 def get_loss(pol, dyn, cost, D, angle_dims, n_samples=50,
-             intermediate_outs=False, resample_particles=True,
+             intermediate_outs=False, resample_particles=True, crn=True,
              resample_dyn=False, average=True, truncate_gradient=-1):
     '''
         Constructs the computation graph for the value function according to
@@ -142,15 +141,32 @@ def get_loss(pol, dyn, cost, D, angle_dims, n_samples=50,
     gamma = tt.scalar('gamma')
 
     # draw initial set of particles
-    z0 = m_rng.normal((n_samples, mx0.size))
+    import numpy as np
+    if crn:
+        # initialize z as a shared variable; i.e. z will be sampled once
+        z = theano.shared(
+            np.random.normal(
+                size=(100, n_samples, D)).astype(theano.config.floatX))
+
+        # now we will make sure that z is has the correct shape
+        z = theano.ifelse.ifelse(
+            z.shape[0] < H,
+            tt.tile(z, (tt.ceil(H/z.shape[0]).astype('int64'), 1, 1)),
+            z
+        )[:H]
+    else:
+        # new samples with every rollout
+        z = m_rng.normal((H, n_samples, D))
+
     Lx0 = tt.slinalg.cholesky(Sx0)
-    x0 = mx0 + z0.dot(Lx0.T)
+    x0 = mx0 + z[0].dot(Lx0.T)
 
     # get rollout output
     r_outs, updts = rollout(x0, H, gamma,
                             pol, dyn, cost,
                             D, angle_dims,
                             resample=resample_particles,
+                            z=z,
                             iid_per_eval=resample_dyn,
                             truncate_gradient=truncate_gradient)
 
