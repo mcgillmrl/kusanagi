5055fc6884c395d763a41fbc2e61d2bdb3d60f48
Juan Camilo Gamboa Higuera <juancamilog@gmail.com>
2017-10-20 16:34:35 -0400

    added extra nonlinearities

---
+++ kusanagi/ghost/regression/nonlinearities.py
@@ -0,0 +1,23 @@
+import theano.tensor as tt
+from lasagne.nonlinearities import *
+
+def silu(x):
+    '''
+    aka  SiL, Swish, etc.
+    '''
+    return x*tt.nnet.sigmoid(x)
+
+def phi(x):
+    return 0.5*(tt.erfc(-x/tt.sqrt(2)))
+
+def gelu(x):
+    '''
+        similar to silu
+    '''
+    return x*phi(x)
+
+def gelu2(x):
+    '''
+        similar to silu
+    '''
+    return x*(tt.erf(x) + 1)
