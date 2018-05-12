eb882109d4ef4d47cf96d649c750878e51219ed0
Juan Camilo Gamboa Higuera <juancamilog@gmail.com>
2018-05-12 10:14:04 -0400

    simplified the gaussian kl loss code

--- kusanagi/shell/cost.py
+++ kusanagi/shell/cost.py
@@ -87,41 +87,30 @@ def quadratic_saturating_loss(mx, Sx, target, Q, *args, **kwargs):
         return 1.0 + m_cost, s_cost
 
 
-def forward_gaussian_kl_loss(t, mx, Sx, target_mean, target_cov,
-                             angle_dims=[], angle_dims_t=[]):
+def empirical_gaussian_params(x):
     '''
-        Returns KL ( Normal(target_mean[t], target_cov[t]) || Normal(mx, Sx) )
+        Returns the empirical mean and covariance of the sample set x
     '''
-    mt, St = target_mean[t], target_cov[t]
-    mx, Sx = convert_angle_dimensions(mx, Sx, angle_dims)
-    mt, St = convert_angle_dimensions(mt, St, angle_dims_t)
+    n = x.shape[0]
+    n = n.astype(theano.config.floatX)
+    mx = x.mean(0)
+    Sx = x.T.dot(x)/n - tt.outer(mx, mx)
+    return mx, Sx
+
+
+def gaussian_kl_loss(mx, Sx, mt, St):
+    '''
+        Returns KL ( Normal(mx, Sx) || Normal(mt, St) )
+    '''
+    if St is None:
+        target_samples = mt
+        mt, St = empirical_gaussian_params(target_samples)
+
     if Sx is None:
-        # TODO evaluate empirical KL
-        return 0
-    else:
-        delta = mx - mt
-        Sxinv = matrix_inverse(Sx)
-        kl = tt.log(det(Sx)) - tt.log(det(St))
-        kl += trace(Sxinv.dot(delta.T.dot(delta) + St - Sx))
-        return 0.5*kl
-
-
-def reverse_gaussian_kl_loss(t, mx, Sx, target_mean, target_cov,
-                             angle_dims=[], angle_dims_t=[]):
-    '''
-        Returns KL ( Normal(mx, Sx) || Normal(target_mean[t], target_cov[t]) )
-    '''
-    mt, St = target_mean[t], target_cov[t]
-    mx, Sx = convert_angle_dimensions(mx, Sx, angle_dims)
-    mt, St = convert_angle_dimensions(mt, St, angle_dims_t)
-    if Sx is None:
-        x = mx
-        n = x.shape[0]
-        n = n.astype(theano.config.floatX)
-        mx = x.mean(0)
-        Sx = x.T.dot(x)/n - tt.outer(mx, mx)
-
         # evaluate empirical KL (expectation over the rolled out samples)
+        x = mx
+        mx, Sx = empirical_gaussian_params(x)
+
         def logprob(x, m, S):
             delta = x - m
             L = cholesky(S)
